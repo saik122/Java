5d357de7a9f968d21c7fb97e23857e84ba459e0c
PENAGALURU SAI KUMAR <124550535+saik122@users.noreply.github.com>
2023-12-21 21:12:49 +0530

    vector

---
+++ collections/Vectorpractice.java
@@ -0,0 +1,19 @@
+package CollectionsPractice;
+
+import java.util.*;
+
+public class Vectorpractice {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        v.add(8);
+        v.add(4);
+        v.add(88);
+        v.add(2);
+        v.add(4);
+        System.out.println(v);
+        System.out.println("vector size : "+v.size());
+        System.out.println("vector capacity : "+v.capacity());
+
+
+    }
+}
